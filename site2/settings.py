d9a72959f6d6437c726581a8dbcf2c936592b155
YK-S <yongkang233@gmail.com>
2020-11-19 22:52:37 +0800

    v1.0

--- site2/settings.py
+++ site2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'a_8tfe)3-lc^9q_#e-ia(d1b=%-tdwyjidwhutu^*-#7*xc4vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -127,3 +127,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'bootstrap'),
     os.path.join(BASE_DIR, 'JavaScript'),
 )
+# 静态文件收集目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
