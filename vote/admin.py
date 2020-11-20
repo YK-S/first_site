dfbbe678f7637037504d453eb0990659c8e60a59
YK-S <yongkang233@gmail.com>
2020-11-20 15:52:55 +0800

    set admin

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from vote import models
 # Register your models here.
+admin.site.register(models.Item)
+admin.site.register(models.Vote)
+admin.site.register(models.User)
